1759fa03d28a58a9bac31508776a2f705e2aabcd
minju412 <59405576+minju412@users.noreply.github.com>
2023-09-04 16:22:33 +0900

    refactor: RequestParam을PathVariable로 변경
    - RESTful 원칙에 따라 리소스에 접근할 때 PathVariable 사용

--- src/main/java/com/example/assignment1/api/PageController.java
+++ src/main/java/com/example/assignment1/api/PageController.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.assignment1.dto.PageRequest;
@@ -27,8 +27,8 @@ public class PageController {
 	}
 
 	// 페이지 정보 조회 (브레드크럼, 서브페이지)
-	@GetMapping()
-	public ResponseEntity<PageResponse.FindResponse> getPageInfo(@RequestParam Long pageId) {
+	@GetMapping("/{pageId}")
+	public ResponseEntity<PageResponse.FindResponse> getPageInfo(@PathVariable Long pageId) {
 		PageResponse.FindResponse findResponse = pageService.getPageInfo(pageId);
 		return new ResponseEntity<>(findResponse, HttpStatus.OK);
 	}
