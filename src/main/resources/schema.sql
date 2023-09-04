3b0f3201c1425d0cb8701daa3ba013c790599ba6
minju412 <59405576+minju412@users.noreply.github.com>
2023-09-04 18:00:26 +0900

    refactor: 스키마 생성 스크립트 수정
    - 데이터베이스 혹은 테이블이 존재하지 않을 때 자동으로 생성하도록 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,13 @@
-CREATE TABLE pages (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    content TEXT,
-    parent_page_id BIGINT,
-    breadcrumbs VARCHAR(255),
-    FOREIGN KEY (parent_page_id) REFERENCES pages(id)
+CREATE DATABASE IF NOT EXISTS `wanted-1`; -- 데이터베이스가 없을 때 생성
+
+USE `wanted-1`; -- 데이터베이스 선택
+
+-- 테이블이 존재하지 않으면 테이블 생성
+CREATE TABLE IF NOT EXISTS pages (
+                                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                     title VARCHAR(255),
+                                     content TEXT,
+                                     parent_page_id BIGINT,
+                                     breadcrumbs VARCHAR(255),
+                                     FOREIGN KEY (parent_page_id) REFERENCES pages(id)
 );
